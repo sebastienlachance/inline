37062fc13785c79f6fe68c0b695bd94fe3cae2a0
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-05-13 06:10:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inline::Application.config.secret_token = '4394f997eb40c6a31bfb7ac04a2afab8aeb8d4223b65cf7e045e93f228f66eedab866d20351f733f006eb441fb29bf8c76f483111386b0974abf1c1544d14a24'
